69b6e74c001fed4d23dd46789174b7c25f123b14
Ashish Kunwar <thaladipesh89@gmail.com>
2025-01-28 20:12:02 +0545

    auth user model

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const User = require("../models/user");
 
 const authRouter = express.Router();
 
-authRouter.post('/api/signup', (res,req)=>{
+authRouter.post("/api/signup", async (req, res) => {
   //get the data from client
   //post the data in the db
   //return that data to the user
- const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
 
+  const existingUser = await User.findOne({
+    email,
+  });
+  if (existingUser) {
+    return res.status(400).json({ msg: "User with same email already exits!" });
+  }
 });
 
-module.exports =authRouter;
+module.exports = authRouter;
