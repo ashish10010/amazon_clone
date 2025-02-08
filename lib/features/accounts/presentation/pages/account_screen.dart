eb7580a0bbedbdb1c94a4a44ebdc7d15aca471ef
Ashish Kunwar <thaladipesh89@gmail.com>
2025-02-08 21:16:37 +0545

    app bar

--- lib/features/accounts/presentation/pages/account_screen.dart
+++ lib/features/accounts/presentation/pages/account_screen.dart
@@ -1,4 +1,5 @@
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/accounts/presentation/widgets/below_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -15,7 +16,48 @@ class AccountScreen extends StatelessWidget {
               gradient: GlobalVariables.appBarGradient,
             ),
           ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/images/amazon_in.png',
+                  width: 120,
+                  height: 145,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 15,
+                  right: 15,
+                ),
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        right: 15,
+                      ),
+                      child: Icon(
+                        Icons.notifications_outlined,
+                      ),
+                    ),
+                    Icon(
+                      Icons.search,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
+      ),
+      body: Column(
+        children: const [
+          BelowAppBar(),
+          SizedBox(height: 10),
+        ],
       ),
     );
   }
