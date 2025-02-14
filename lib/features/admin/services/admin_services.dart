77e8fb8fc9e66f05cc1f5aefd7921a8a9debd3e9
Ashish Kunwar <thaladipesh89@gmail.com>
2025-02-15 00:46:26 +0545

    admin-selling product(product model, admin_service)

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:amazon_clone/constants/utils.dart';
+import 'package:amazon_clone/models/product.dart';
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,22 @@ class AdminServices {
   }) async {
     try {
       final cloudinary = CloudinaryPublic('dm88qwpcr', 'cesyvywm');
+      List<String> imageUrls = [];
+
+      for (int i = 0; i < images.length; i++) {
+        CloudinaryResponse res = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(images[i].path, folder: name),
+        );
+        imageUrls.add(res.secureUrl);
+      }
+      Product product = Product(
+        name: name,
+        description: description,
+        quantity: quantity,
+        images: imageUrls,
+        category: category,
+        price: price,
+      );
     } catch (e) {
       showSnackBar(context, e.toString());
     }
